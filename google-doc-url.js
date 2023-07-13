08bcb56bb85c9ed3c42ce183e315d75c70b65225
elishevapicker <elishevapicker@gmail.com>
2023-07-13 13:56:27 +1000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1tI0VF6FyhoBtqFnY2a1bcI32SoPaWZ5PgplAyxEtLo8/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1XcwppF0yWMfj7oV728BcA7AhF9LbzJORoxIjw6un6HY/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
